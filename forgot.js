f89c3b9ddabef108520ab9e6e5595bf169431ab5
RIshabh-Raj4272 <deathkingpro27@gmail.com>
2026-05-26 21:24:38 +0530

    Add files via upload

---
+++ forgot.js
@@ -0,0 +1,7 @@
+function codeSended() {
+alert("Verification code sended! Check your G-Mail and enter the code!")
+
+setTimeout(function() {
+    window.location.href = "forgot2.html";
+    }, 1000); //1s delay 
+}
---
+++ login.js
@@ -0,0 +1,3 @@
+function logIn() {
+alert("Logged In! Check in browser or app to confirm if account is logged in.")
+}
